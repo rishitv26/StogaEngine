bfc8a43405bf1e71c385056954c6a6a4c07b532f
rishitv26 <varshneyrishit5@gmail.com>
2024-05-12 22:21:02 -0400

    working on new version, a lot more organized

--- include/stogaEngine/robot.h
+++ include/stogaEngine/robot.h
@@ -14,7 +14,20 @@
 
 namespace engine {
 class Robot {
-protected: 
+protected:
+	/*
+	* The Chassis Object:
+	* Represents the Chassis of the robot as a set of motors to control.
+	* Also Interfaces Engine with motors in order to move robot during autons.
+	**/
+	engine::Chassis chassis;
+
+	/*
+	* The Utilities Object:
+	* Represents all other components of the robot (ex. lift, intake, etc.)
+	* Also Interfaces Engine with these components in order to move robot during autons.
+	**/
+	engine::Utilities utilities;
 public:
 };
 };
